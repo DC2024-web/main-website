d4b4598edd072e1701135478389211ece92c7e12
Jonathan Siew <jonathansiew.2004@gmail.com>
2024-11-03 22:30:27 +0800

    2nd Draft Upcoming Event Schedule

--- dc2024/app/components/UpcomingEvents/EventCard.tsx
+++ dc2024/app/components/UpcomingEvents/EventCard.tsx
@@ -1,4 +1,4 @@
-// File: UpcomingEvents/EventCard.tsx
+// EventCard.tsx
 import React from 'react';
 import styles from './EventCard.module.css';
 
@@ -6,19 +6,29 @@ type EventCardProps = {
   title: string;
   departments: string;
   date: string;
+  month: string;
   time: string;
 };
 
-const EventCard: React.FC<EventCardProps> = ({ title, departments, date, time }) => {
+const EventCard: React.FC<EventCardProps> = ({ 
+  title, 
+  departments, 
+  date, 
+  month, 
+  time 
+}) => {
   return (
-    <div className={styles.card}>
-      <div className={styles.infoSection}>
-        <h3 className={styles.title}>{title}</h3>
-        <p className={styles.departments}>{departments}</p>
-      </div>
-      <div className={styles.dateSection}>
-        <h2 className={styles.date}>{date}</h2>
-        <p className={styles.time}>{time}</p>
+    <div>
+      <div className={styles.card}>
+        <div className={styles.infoSection}>
+          <h3 className={styles.title}>{title}</h3>
+          <p className={styles.departments}>{departments}</p>
+        </div>
+        <div className={styles.dateSection}>
+          <div className={styles.date}>{date}</div>
+          <div className={styles.month}>{month}</div>
+          <div className={styles.time}>{time}</div>
+        </div>
       </div>
       <button className={styles.venueButton}>SEE VENUE</button>
     </div>
