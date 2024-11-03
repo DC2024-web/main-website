dc52be2b99f40853073e6492cb674f5d95b47034
Jonathan Siew <jonathansiew.2004@gmail.com>
2024-11-03 14:33:04 +0800

    Fixed Parameter 'url' implicitly has an 'any' type error

--- dc2024/app/components/Footer/Footer.tsx
+++ dc2024/app/components/Footer/Footer.tsx
@@ -20,9 +20,9 @@ const links = {
 };
 
 const Footer = () => {
-  const openLink = (url) => {
+  const openLink = (url: string) => {
     window.open(url, "_blank", "noopener,noreferrer");
-  };
+  };  
 
   return (
     <>
